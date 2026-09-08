1c94c3b41c6dcf0cf0fe57cf0cab258aa6327d3a
WhisperingRock <xpropellorheadx@gmail.com>
2026-09-08 13:47:19 -0700

    Move kernel vars onto Conv plus sobel func

--- 02_hardware/sim/Convolution_tb.sv
+++ 02_hardware/sim/Convolution_tb.sv
@@ -31,34 +31,19 @@ module Convolution_tb();
 	logic					clk;
 	logic [71:0]			mat;
 	logic					mat_valid;
-	logic [7:0]				result1, result2; 
-	logic					result_valid1, result_valid2;
+	logic [7:0]				result1, 		result2; 
+	logic					result_valid1,	result_valid2;
 	// ~~ consts ~~
-	localparam logic signed [7:0] K_BOX [0:8] = 
-	'{
-		1, 1, 1,
-		1, 1, 1,
-		1, 1, 1
-	};
-	
-	localparam logic signed [7:0] K_SOBEL_X [0:8] = 
-	'{
-		1, 0, -1,
-		2, 0, -2,
-		1, 0, -1
-	};
-	
-	
 	// ~~ testing ~~
 	testcase tc;
 	logic	[31:0]			tnum;
 	
 	
 	// ~~~~ module instances ~~~~
+	// ~~ box blur ~~
 	Convolution 
 	#(
-		.KERNEL(K_BOX), 
-		.DIV(9)
+		.FILTER(1'b0)
 	)
 	UUT_BOX
 	(
@@ -68,11 +53,10 @@ module Convolution_tb();
 		.RESULT_o(result1), 
 		.RESULT_VALID_o(result_valid1)
 	);
-	
+	// ~~ sobel ~~
 	Convolution 
 	#(
-		.KERNEL(K_SOBEL_X), 
-		.DIV(1)
+		.FILTER(1'b1)
 	)
 	UUT_SOBEL_X
 	(
@@ -90,7 +74,6 @@ module Convolution_tb();
 		clk	<= !clk;  
 	end
 
-	
 	// ~~~~ testing ~~~~
 	initial begin
 	
@@ -108,12 +91,12 @@ module Convolution_tb();
 		tc.new_test("1's Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h01_01_01___01_01_01___01_01_01;
-			#5;
-			assert(result1 === {8'h01})					else tc.err("BoxBlur result error");
-			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid1 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			#11;
+			assert(result1 			=== {8'h01})		else tc.err("BoxBlur result error");
+			assert(result_valid1 	=== {1'b1})			else tc.err("BoxBlur result_valid error");
+			assert(result2 			=== {8'h00})		else tc.err("Sobel result error");
+			assert(result_valid1 	=== {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 				
@@ -121,96 +104,96 @@ module Convolution_tb();
 		tc.new_test("Floor Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h00_00_00___00_00_00___00_00_00;
-			#5;
+			#11;
 			assert(result1 === {8'h00})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC3 ~~
 		tc.new_test("5's Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h05_05_05___05_05_05___05_05_05;
-			#5;
+			#11;
 			assert(result1 === {8'h05})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC4 ~~
 		tc.new_test("Ceiling Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'hFF_FF_FF___FF_FF_FF___FF_FF_FF;
-			#5;
+			#11;
 			assert(result1 === {8'hFF})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC5 ~~
 		tc.new_test("Identity Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h01_00_00___00_01_00___00_00_01;
-			#5;
+			#11;
 			assert(result1 === {8'h00})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC6 ~~
 		tc.new_test("3x Identity Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h03_00_00___00_03_00___00_00_03;
-			#5;
+			#11;
 			assert(result1 === {8'h01})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC7 ~~
 		tc.new_test("Increasing Mat"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h01_02_03___04_05_06___07_08_09; // mat[0] = 09 
-			#5;
+			#11;
 			assert(result1 === {8'h05})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h08})					else tc.err("SobelX result error");				
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");				
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC8 ~~
 		tc.new_test("Increasing Mat Reversed"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h09_08_07___06_05_04___03_02_01;	// mat[0] = 01
-			#5;
+			#11;
 			assert(result1 === {8'h05})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'h00})					else tc.err("SobelX result error");				// should be -8 but floor clip
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'h00})					else tc.err("Sobel result error");				// should be -8 but floor clip
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC9 ~~
 		tc.new_test("Single-Centered 9"); 
 		tnum					= tc.get_testnum();
 			mat					= 72'h00_00_00___00_00_09___00_00_00;
-			#5;
+			#11;
 			assert(result1 === {8'h01})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'd18})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b1})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'd00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
 		
 		// ~~ TC10 ~~
@@ -218,12 +201,39 @@ module Convolution_tb();
 		tnum					= tc.get_testnum();
 			mat					= 72'h00_00_00___00_09_00___00_00_00;
 			mat_valid	= 1'b0;
-			#5;
+			#11;
 			assert(result1 === {8'h01})					else tc.err("BoxBlur result error");
 			assert(result_valid1 === {1'b0})			else tc.err("BoxBlur result_valid error");
-			assert(result2 === {8'd18})					else tc.err("SobelX result error");
-			assert(result_valid2 === {1'b0})			else tc.err("SobelX result_valid error");
-			#5;
+			assert(result2 === {8'd00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b0})			else tc.err("Sobel result_valid error");
+			#9;
 		tc.test_done();
+		
+		// ~~ TC11 ~~
+		tc.new_test("Medium Range Numbers"); 
+		tnum					= tc.get_testnum();
+			mat					= 72'h81_81_81___81_81_81___81_81_81;
+			mat_valid	= 1'b1;
+			#11;
+			assert(result1 === {8'h81})					else tc.err("BoxBlur result error");
+			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
+			assert(result2 === {8'd00})					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
+		tc.test_done();
+		
+		// ~~ TC12 ~~
+		tc.new_test("Sobel Numbers"); 
+		tnum					= tc.get_testnum();
+			mat					= 72'h00_00_81___00_00_81___81_81_81;
+			mat_valid	= 1'b1;
+			#11;
+			assert(result1 === {8'd71})					else tc.err("BoxBlur result error");
+			assert(result_valid1 === {1'b1})			else tc.err("BoxBlur result_valid error");
+			assert(result2 === 8'hFF)					else tc.err("Sobel result error");
+			assert(result_valid2 === {1'b1})			else tc.err("Sobel result_valid error");
+			#9;
+		tc.test_done();
+		
 	end
 endmodule
--- 02_hardware/src/Convolution.sv
+++ 02_hardware/src/Convolution.sv
@@ -22,27 +22,51 @@
 
 module Convolution
 #(
-	parameter logic signed [7:0] 	KERNEL [0:8] = 
+	parameter logic			FILTER = 0
+)
+(
+	input logic						CLK_i, 
+	input logic		[71:0]			MATRIX_i,				// 3x3 = 9 pixels, 8 bits each
+	input logic						MATRIX_VALID_i, 		// input ready for import
+	output logic	[7:0]			RESULT_o, 					
+	output logic					RESULT_VALID_o			// result ready for export
+);
+
+	// ~~~~ local param/alloc/const ~~~~
+
+	localparam signed	[8:0] 		pixelmin = 9'b0_0000_0000;
+	localparam signed 	[8:0]		pixelmax = 9'b0_1111_1111;
+
+	// box blurr
+	logic signed 		[23:0] 		sum_comb;
+	logic signed 		[23:0] 		temp_result;
+	logic 				[15:0]		div;
+	assign 							div = (FILTER == 0) ? 16'd9 : 16'd1; 
+	
+	localparam logic signed [7:0] 	K_BOX [0:8] = 
 	'{
 		8'd1, 8'd1, 8'd1,
 		8'd1, 8'd1, 8'd1,
 		8'd1, 8'd1, 8'd1		
-	},
+	};
 	
-	parameter logic [15:0]		 	DIV = 16'd9
-)
-(
-	input logic				CLK_i, 
-	input logic		[71:0]	MATRIX_i,				// 3x3 = 9 pixels, 8 bits each
-	input logic				MATRIX_VALID_i, 		// input ready for import
-	output logic	[7:0]	RESULT_o, 					
-	output logic			RESULT_VALID_o			// result ready for export
-);
+	// sobel
+	logic signed 		[23:0] 		sum_x, sum_y;
+	logic signed 		[23:0] 		tmp_x, tmp_y;
+	localparam signed	[23:0]		sobel_thresh = 4000;
+	localparam logic signed [7:0] 	K_SOBEL_X [0:8] = 
+	'{
+		1, 0, -1,
+		2, 0, -2,
+		1, 0, -1
+	};
+	localparam logic signed [7:0] 	K_SOBEL_Y [0:8] = 
+	'{
+		 1,  2,  1,
+		 0,  0,  0,
+		-1, -2, -1
+	}; 
 
-	// ~~~~ local param/alloc/const ~~~~
-	logic signed 		[23:0] 	sum_comb;
-	localparam signed	[8:0] 	PIXELMIN = 0;
-	localparam signed 	[8:0]	PIXELMAX = 255; 
 
 	
 	// ~~~~ async logic ~~~~
@@ -50,14 +74,28 @@ module Convolution
 	always_comb begin
 		
 		// ~~ sanitize between kernel operations ~~
-		sum_comb = '0;
+		sum_comb 	= '0;
+		sum_x		= '0;
+		sum_y		= '0;
 		
-		// ~~ compute an entire kernel sum per CLK ~~
-		for(int i = 0; i < 9; i++) begin
-			sum_comb += KERNEL[i] * $signed({1'b0, MATRIX_i[i*8 +: 8]});		// idx part-selection 
-																				//     [start_index +: width]
-																				// extending to signed
+		// ~~ box blurr filter ~~
+		if(FILTER == 0) begin
+			// ~~ compute kernel sum per CLK ~~
+			for(int i = 0; i < 9; i++) begin
+				sum_comb += K_BOX[i] * $signed({1'b0, MATRIX_i[i*8 +: 8]});																						
+			end
 		end
+		
+		// ~~ sobel filter ~~
+		else begin
+			// ~~ compute kernel sum per CLK ~~
+			for(int i = 0; i < 9; i++) begin
+				sum_x += K_SOBEL_X[i] * $signed({1'b0, MATRIX_i[i*8 +: 8]});
+				sum_y += K_SOBEL_Y[i] * $signed({1'b0, MATRIX_i[i*8 +: 8]});																						
+			end
+		end
+		
+		
 	end
 	
 	
@@ -67,9 +105,29 @@ module Convolution
 		RESULT_VALID_o <= MATRIX_VALID_i;										// result likely ready if input is 
 		
 		if(MATRIX_VALID_i) begin
-			if(sum_comb < PIXELMIN) 		begin 	RESULT_o <= 8'd0; 			end	// floor clip 
-			else if(sum_comb > PIXELMAX) 	begin	RESULT_o <= PIXELMAX; 		end	// or ceil clip
-			else 							begin	RESULT_o <= sum_comb / DIV;	end	// or apply gain factor
+		
+			// box filter
+			if(FILTER == 0) begin
+				temp_result <= sum_comb / div;
+				
+				// pixel clipping
+				if(temp_result < pixelmin) 			begin 	RESULT_o <= 8'd0; 			end	// floor clip 
+				else if(temp_result > pixelmax) 	begin	RESULT_o <= pixelmax; 		end	// or ceil clip
+				else 								begin	RESULT_o <= temp_result;	end	// or apply gain factor
+			end
+			
+			// sobel filter
+			else begin
+						
+				tmp_x <= $signed(sum_x) * $signed(sum_x); // Gx ^ 2
+				tmp_y <= $signed(sum_y) * $signed(sum_y); // Gy ^ 2
+				temp_result <= $signed(tmp_x) + $signed(tmp_y); // Gx^2 + Gy^2
+				
+				// Avoid square root and use threshold instead 
+				if(temp_result < sobel_thresh)	begin 	RESULT_o <= pixelmin; 	end	 
+				else  							begin	RESULT_o <= pixelmax;	end
+				
+			end
 		end
 	end
 
